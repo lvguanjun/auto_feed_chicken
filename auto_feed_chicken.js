b5b87552164c54efb36f166a7ed813df6c378039
lvguanjun <1193965453@qq.com>
2021-05-16 19:34:31 +0800

    refactor: refactor repo
    使用不方便的地方，逻辑重构及其他
    1. 当前优先判断是否庄园，避免从庄园页面回退首页后重新进入庄园
    2. 非锁屏界面弹窗提示是否喂鸡（多次打断游戏）
    3. 当前应用为搜狗输入法时，还原环境时直接回到home界面（误判断）
    4. 增加思维导图
    
    Signed-off-by: lvguanjun <1193965453@qq.com>

--- auto_feed_chicken.js
+++ auto_feed_chicken.js
@@ -1,5 +1,87 @@
+function selfUnlock(passwd) {
+    lock_flag = true;
+    current_app = null;
+    if (device.isScreenOn()) {
+        lock_flag = false;
+        current_app = app.getAppName(currentPackage());
+        if (!dialogs.confirm('auto_feed_chicken', '当前应用为' + current_app + ', 是否自动喂鸡')) {
+            exit();
+        }
+    } else {
+        var i = 0;
+        do {
+            device.wakeUpIfNeeded();
+            sleep(1000);
+            swipe(720, 3100, 720, 1100, 350);
+            i++;
+            if (i > 5) {
+                toastLog('解锁界面进入失败');
+                exit();
+            }
+        } while (!desc(1).findOnce())
+        clickPasswd(passwd);
+    }
+    return [lock_flag, current_app];
+}
+
+function clickPasswd(passwd) {
+    for (i = 0; i < passwd.length; i++) {
+        desc(Number(passwd.charAt(i))).findOnce().click();
+        sleep(500);
+    }
+}
+
+function enterManor() {
+    var i = 0;
+    while (app.getAppName(currentPackage()) != '支付宝') {
+        launchApp('支付宝');
+        i++;
+        sleep(500);
+        if (i > 5) {
+            toastLog('启动支付宝失败');
+            exit();
+        }
+    }
+    if (!judgeManor()) {
+        var i = 0;
+        while (!judgeMainPage()) {
+            back();
+            if (judgeManor()) {
+                return;
+            }
+            sleep(1000);
+            i++;
+            if (i > 10) {
+                toastLog('进入支付包首页失败');
+                exit();
+            }
+        }
+        var manor = text('蚂蚁庄园').findOnce();
+        while (manor.parent()) {
+            if (manor.parent().clickable()) {
+                manor.parent().click();
+                return;
+            }
+            manor = manor.parent();
+        }
+        toastLog('进入庄园失败');
+        exit();
+    }
+}
+
+function judgeManor() {
+    for (var i = 1; i <= 3; i++) {
+        console.log('第' + i + '次校验庄园');
+        sleep(500);
+        if (text('蚂蚁庄园').exists() && desc('更多').exists() && desc('关闭').exists()) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function judgeMainPage() {
-    for (var i = 1; i <= 5; i++) {
+    for (var i = 1; i <= 3; i++) {
         sleep(300);
         console.log('第' + i + '次校验是否主页');
         if (text('扫一扫').exists() && text('付钱/收钱').exists() && text('出行').exists() && text('首页').exists()) {
@@ -12,118 +94,30 @@ function judgeMainPage() {
     return false;
 }
 
-function enterMainPage() {
-    var i = 0;
-    while (currentPackage() != getPackageName('支付宝')) {
-        launchApp('支付宝');
-        if (i > 5) {
-            toastLog('5次启动支付宝失败');
-            device.vibrate(2000);
-            exit();
-        }
-        i++;
-        sleep(500);
-    }
-    for (var i = 0; i < 10; i++) {
-        if (judgeMainPage())
-            return true;
-        back();
-        sleep(1000);
-    }
-    return false;
-}
-
-function enterManor() {
-    var manor = text('蚂蚁庄园').findOnce();
-    while (manor.parent()) {
-        if (manor.parent().clickable()) {
-            manor.parent().click();
-            return true;
-        }
-        manor = manor.parent();
-    }
-    return false;
-}
-
-function judgeManor() {
-    for (var i = 1; i <= 5; i++) {
-        console.log('第' + i + '次校验庄园');
-        sleep(500);
-        if (text('蚂蚁庄园').exists() && desc('更多').exists() && desc('关闭').exists()) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function clickPasswd(passwd) {
-    for (i = 0; i < passwd.length; i++) {
-        desc(Number(passwd.charAt(i))).findOnce().click();
-        sleep(500);
-    }
-}
-
-
-function unlock() {
-    if (!device.isScreenOn()) {
-        var i = 0;
-        //点亮屏幕
-        while (!device.isScreenOn()) {
-            device.wakeUp();
-            //由于MIUI的解锁有变速检测，因此要点开时间以进入密码界面
-            sleep(1000);
-            i++;
-            if (i > 10) {
-                toastLog('无法唤醒屏幕');
-                device.vibrate(2000);
-                exit();
-            }
-        }
-        var j = 0;
-        do {
-            j++;
-            swipe(720, 3100, 720, 1100, 350);
-            sleep(500);
-            if (j > 5) {
-                toastLog("解锁界面开启失败");
-                exit();
-            }
-        } while (!desc(1).findOnce())
-        //输入屏幕解锁密码
-        clickPasswd('123456');
-        return true;
-    }
-    return false;
-}
-
 function main() {
     setScreenMetrics(1440, 3200);
-    var lock_flag = 0;
-    if (unlock()) {
-        lock_flag = 1;
+    var passwd = '123456';
+    let [lock_flag, current_app] = selfUnlock(passwd);
+    enterManor();
+    sleep(2000);
+    press(1250, 2950, 350);
+    sleep(2000);
+    if (lock_flag) {
+        home();
+        sleep(2000);
+        quickSettings();
+        sleep(1000);
+        desc('锁屏').findOnce().parent().click();
     } else {
-        now_app = currentPackage();
-    }
-    if (enterMainPage()) {
-        enterManor();
-        if (judgeManor()) {
-            sleep(2000);
-            press(1250, 2950, 350);
-            sleep(2000);
-            home();
-            sleep(1000)
-            if (lock_flag == 0) {
-                var i = 0;
-                do {
-                    launch(now_app);
-                    i++;
-                    sleep(200);
-                } while (currentPackage() != now_app && i < 5)
-                return;
-            }
-            quickSettings();
-            sleep(1000);
-            desc('锁屏').findOnce().parent().click();
+        switch (current_app) {
+            case '搜狗输入法':
+            case '系统桌面':
+                home();
+                break;
+        }
+        for (var i = 0; i < 5 && app.getAppName(currentPackage()) != current_app; i++) {
+            launchApp(current_app);
+            sleep(500);
         }
     }
 }
